bdd988ded9adf26224beea26de4ef4a6c900371e
nbdehlen <nbdehlen@gmail.com>
2019-11-05 01:41:19 +0100

    general ideas of how to implement a hangman game

--- js/script.js
+++ js/script.js
@@ -17,3 +17,48 @@ let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+
+/*
+*** IDEAS AND FEATURES***
+Sound?
+Html canvas?
+Animations?
+Reset button?
+clues with multiple words?
+instructions?
+
+*** Things that needs to happen for the game to work ***
+
+*Button click to start the game
+    - Function for choosing a random word
+        - Making an array for the letter spaces for that random word
+            - Displaying the letter spaces amount based on random word
+
+*General
+    - Store correct guesses            
+    - Store incorrect guesses
+
+*Button click to guess letters matching the word            
+   - Link button to letter
+        - If letter match random word
+            - update it in stored correct guesses
+            - display on letter spaces
+            - display greyed out on keyboard-buttons
+
+        - If letter does not match
+            - update it in stored incorrect guesses
+            - display greyed out on keyboard-buttons
+            - update hangman image
+
+    - Did guesses match all letters in the random word?
+        - end game with message that you won
+
+    - Did guesses > amount of guesses allowed?
+        - end game with message that you lost
+
+
+*Button for resetting the game
+
+
+
+*/
